0b3c926d4c9b243939f8a7c30d2ed8851e51237e
Marymezh <maria.mezhova@yahoo.com>
2022-10-26 10:48:21 +0500

    Create table view for ToDoey Items with add new item button

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    private var itemArray = ["AAAA", "BBB", "CCC"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK - TableView Datasource and Delegate Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+        }
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            if let newItemName = alert.textFields?[0].text,
+               newItemName != "" {
+                self.itemArray.append(newItemName)
+                self.tableView.reloadData()
+            } else {
+                self.showErrorAlert(text: "You forgot to enter new item name!")
+            }
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func showErrorAlert(text: String) {
+        let alert = UIAlertController(title: "Error", message: text, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+}
+
